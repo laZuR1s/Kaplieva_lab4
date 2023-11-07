ab11348f4bffb3c80491e534c664a0a12ab7cf6e
laZuR1s <sashalazyr@gmail.com>
2023-11-07 23:11:20 +0300

    Конечные изменения

--- laba4/laba4.cpp
+++ laba4/laba4.cpp
@@ -8,6 +8,7 @@ int check_file(std::ifstream& file);
 void ending(int n);
 template <typename T, typename Predicat>
 void Read_and_check(T& x, Predicat condition, const char* message);
+int GetRandomInInterval(int a, int b);
 
 //Менюшки
 int main_menu();
@@ -18,9 +19,9 @@ int task1(int size, std::istream& stream);
 int task1(int size, int a, int b);
 
 //Вторая задача
-int task2(int size,int &count, int endNumb, std::istream& stream,std::function<bool(int)> predicate);
+int task2(int size, int& count, int endNumb, std::istream& stream, std::function<bool(int)> predicate);
 void print_answer(int ind, int res);
-int task2(int size, int& count, int endNumb,int a, int b, std::function<bool(int)> predicate);
+int task2(int size, int& count, int endNumb, int a, int b, std::function<bool(int)> predicate);
 
 int main()
 {
@@ -50,16 +51,15 @@ int main()
 							int endNumb;
 							int count = 0;
 							std::cout << "\nВведите цифру окончания числа: ";
-							Read_and_check(endNumb, [](int x) {return x > 0&&x<10; }, "\n-> ");
+							Read_and_check(endNumb, [](int x) {return x > 0 && x < 10; }, "\n-> ");
 
-							int res = task2(size,count, endNumb, std::cin, [endNumb](int x) {return abs(x) % 10 == endNumb; });
-							print_answer(res,count);
+							int res = task2(size, count, endNumb, std::cin, [endNumb](int x) {return abs(x) % 10 == endNumb; });
+							print_answer(res, count);
 						}
 					}
 					if (choice == 2)
 					{
-
-						std::ifstream file("data.txt");
+						std::ifstream file("task_universal.txt");
 						switch (check_file(file))
 						{
 						case -1:
@@ -102,7 +102,7 @@ int main()
 						Read_and_check(b, [](int x) {return true; }, "");
 
 						if (mainChoice == 1)
-							std::cout << "\nНомер первого максимального элемента: " << task1(size, a,b) << '\n';
+							std::cout << "\nНомер первого максимального элемента: " << task1(size, a, b) << '\n';
 						else
 						{
 							int endNumb;
@@ -111,7 +111,7 @@ int main()
 							std::cout << "\nВведите цифру окончания числа: ";
 							Read_and_check(endNumb, [](int x) {return x > 0 && x < 10; }, "\n-> ");
 
-							int res = task2(size, count, endNumb,a,b, [endNumb](int x) {return abs(x) % 10== endNumb; });
+							int res = task2(size, count, endNumb, a, b, [endNumb](int x) {return abs(x) % 10 == endNumb; });
 							print_answer(res, count);
 						}
 					}
@@ -156,6 +156,12 @@ void ending(int n)
 		}
 	}
 }
+
+int GetRandomInInterval(int a, int b)
+{
+	return a + rand() % (b - a + 1);
+}
+
 int main_menu()
 {
 	std::cout << "\n--------------\n";
@@ -170,16 +176,17 @@ int main_menu()
 			return x >= 1 && x <= 3;
 		};
 	int choice = 0;
-	Read_and_check(choice, Lambda, "\n->");
+	Read_and_check(choice, Lambda, "\n-> ");
 	return choice;
 }
+
 int choice_menu()
 {
 	std::cout << "\n--------------\n";
 	std::cout << "\n1. Ввод чисел с клавиатуры" << '\n';
 	std::cout << "2. Ввод чисел из файла" << '\n';
 	std::cout << "3. Случайный набор чисел" << '\n';
-	std::cout << "4. Закрыть решение задачи" << '\n';
+	std::cout << "4. Выйти в главное меню" << '\n';
 	std::cout << "\n--------------\n";
 
 	std::function<bool(int)> Lambda = [](int x)->bool
@@ -203,7 +210,7 @@ int task1(int size, std::istream& stream)
 		Read_and_check(max, [](int x) {return true; }, "\n");
 		for (int i = 2; i <= size; ++i)
 		{
-			Read_and_check(x, [](int x) {return true; },"");
+			Read_and_check(x, [](int x) {return true; }, "");
 			if (x > max)
 			{
 				max = x;
@@ -232,13 +239,13 @@ int task1(int size, int a, int b)
 {
 
 	int x;
-	int max = a + rand() % (b - a + 1);
+	int max = GetRandomInInterval(a, b);
 	std::cout << "\nСлучайные элементы: ";
 	std::cout << max << ' ';
 	int numb = 1;
 	for (int i = 2; i <= size; i++)
 	{
-		x = a + rand() % (b - a + 1);
+		x = GetRandomInInterval(a, b);
 		std::cout << x << ' ';
 		if (x > max)
 		{
@@ -252,13 +259,13 @@ int task1(int size, int a, int b)
 	return numb;
 }
 
-int task2(int size, int &count, int endNumb, std::istream& stream, std::function<bool(int)> predicate)
+int task2(int size, int& count, int endNumb, std::istream& stream, std::function<bool(int)> predicate)
 {
 
 	int x;
 	int flag = 0;
 	int result = 2;
-	if(&stream==&std::cin)
+	if (&stream == &std::cin)
 	{
 		ending(size);
 
@@ -267,7 +274,6 @@ int task2(int size, int &count, int endNumb, std::istream& stream, std::function
 			Read_and_check(x, [](int x) {return true; }, "");
 			if (!flag)
 			{
-
 				if (predicate(x))
 					flag = i;
 			}
@@ -292,7 +298,7 @@ int task2(int size, int &count, int endNumb, std::istream& stream, std::function
 					count++;
 		}
 	}
-	if (!flag)
+	if (!flag || count == 0)
 		result = -1;
 	else
 		if (flag == size)
@@ -309,41 +315,40 @@ int task2(int size, int& count, int endNumb, int a, int b, std::function<bool(in
 	int result = 2;
 
 	std::cout << "\nСлучайные элементы: ";
-	
-		for (int i = 1; i <= size; ++i)
-		{
-			x = a + rand() % (b - a + 1);
-			std::cout << x << ' ';
-			if (!flag)
-			{
 
-				if (predicate(x))
-					flag = i;
-			}
-			else
-				if (x > 0)
-					count++;
-		}
-		std::cout << '\n';
+	for (int i = 1; i <= size; ++i)
+	{
+		x = GetRandomInInterval(a, b);
+		std::cout << x << ' ';
 		if (!flag)
-			result = -1;
+		{
+
+			if (predicate(x))
+				flag = i;
+		}
 		else
-			if (flag == size)
-				result = 0;
+			if (x > 0)
+				count++;
+	}
+	std::cout << '\n';
+	if (!flag || count == 0)
+		result = -1;
+	else
+		if (flag == size)
+			result = 0;
 
 	return result;
 }
+
 void print_answer(int ind, int res)
 {
 	switch (ind)
 	{
 	case -1: std::cout << "Нет элементов с заданным свойством\n";
 		break;
-	case 1: std::cout << "Только один элемент с заданным свойством\n";
-		break;
 	case 0: std::cout << "Пустой диапазон\n";
 		break;
-	default: std::cout << "Результат равен: " << res << '\n';
+	default: std::cout << "\nРезультат равен: " << res << '\n';
 	}
 }
 
